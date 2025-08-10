3a3ca90093bbd7b86385c2ee3946961ce9ce04d7
DaniyalAsif7778 <daniyal.personel7778@gmail.com>
2025-08-10 13:37:46 +0500

    change folder structure

--- frontend/app/importexport.js
+++ frontend/app/importexport.js
@@ -1,5 +1,1 @@
-import { HeaderMegaMenu } from "../components/header/LandingPageHeader";
- import { FooterSimple } from "@/components/footer/FooterSimple";
- import Converter from "@/components/HomeComponent/Converter";
- import Features from "@/components/others/Features";
- export {HeaderMegaMenu , FooterSimple,Converter,Features};
+ 
